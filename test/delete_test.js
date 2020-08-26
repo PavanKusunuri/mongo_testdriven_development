738e957cfd6062af6df5acd393aeee3f492f4f99
Pavan <pavantejakusunuri@gmail.com>
2020-08-26 08:37:58 +0530

    Modal instance remove method for removing user

---
+++ test/delete_test.js
@@ -0,0 +1,18 @@
+const assert = require("assert");
+const User = require("../src/user");
+
+describe("Deleting a user", () => {
+  beforeEach((done) => {
+    joe = new User({ name: "Joe" });
+    joe.save()
+      .then(() => done());
+  });
+  it("model instance remove", (done) => {
+    joe.remove()
+      .then(() => User.findOne({ name: "Joe" }))
+      .then((user) => {
+        assert(user === null);
+        done();
+      });
+  });
+});
